0d2cb1f8b646f3ec962d97c96d3a6bf9be6aebc4
Sagar-Patil-C94 <sagarp@ivoyant.com>
2023-12-29 11:22:42 +0530

    Used Both Type Of DI With Map

--- src/main/java/com/ivoyant/di/Bike.java
+++ src/main/java/com/ivoyant/di/Bike.java
@@ -1,38 +1,40 @@
 package com.ivoyant.di;
 
+import java.util.Map;
+
 public class Bike {
     private String name;
-    private int cc;
+    private Map<String, String> bikeDetails;
 
-    public Bike() {
-    }
-
-    public Bike(String name, int cc) {
-        this.name = name;
-        this.cc = cc;
+    @Override
+    public String toString() {
+        return "Bike{" +
+                "name='" + name + '\'' +
+                ", bikeDetails=" + bikeDetails +
+                '}';
     }
 
     public String getName() {
         return name;
     }
 
-    @Override
-    public String toString() {
-        return "Bike{" +
-                "name='" + name + '\'' +
-                ", cc=" + cc +
-                '}';
-    }
-
     public void setName(String name) {
         this.name = name;
     }
 
-    public int getCc() {
-        return cc;
+    public Map<String, String> getBikeDetails() {
+        return bikeDetails;
     }
 
-    public void setCc(int cc) {
-        this.cc = cc;
+    public void setBikeDetails(Map<String, String> bikeDetails) {
+        this.bikeDetails = bikeDetails;
+    }
+
+    public Bike() {
+    }
+
+    public Bike(String name, Map<String, String> bikeDetails) {
+        this.name = name;
+        this.bikeDetails = bikeDetails;
     }
 }

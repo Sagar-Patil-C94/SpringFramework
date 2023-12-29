2b0042891ce06448dbe6b439d6a93c9e71857b43
Sagar-Patil-C94 <sagarp@ivoyant.com>
2023-12-29 12:52:21 +0530

    Bean Cycle

---
+++ src/main/java/com/ivoyant/beanCycle/ExtensionBean.java
@@ -0,0 +1,16 @@
+package com.ivoyant.beanCycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class ExtensionBean implements InitializingBean, DisposableBean {
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Extension Bean Is Destroying");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Extension Bean Initializing");
+    }
+}
---
+++ src/main/java/com/ivoyant/beanCycle/XmlBean.java
@@ -0,0 +1,10 @@
+package com.ivoyant.beanCycle;
+
+public class XmlBean {
+    public void init(){
+        System.out.println("XML Bean Init Function!!!");
+    }
+    public void destroy(){
+        System.out.println("XML Bean is destroying!!");
+    }
+}
